045061aa944a063ba5ca80e02cc794f91547224e
Daniela Fernandez <daniela.fernandez7@gmail.com>
2019-03-24 22:46:07 -0300

    Third commit: Section2 Mi expetiencia y Section3 Mis últimos trabajos

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+$(function(){
+
+  $('.carousel').carousel({
+  interval: 2000
+  });
+  
+  $("a").click(function(event){
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var gato = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(gato).offset().top
+      }, 800, function(){
+        window.location.hash = gato;
+      });
+    }
+  });
+
+  $('[data-toggle="popover"]').popover();
+
+});
