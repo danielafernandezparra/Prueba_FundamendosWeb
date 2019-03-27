5b95684a9e946f6d7357770bfe59d3c2dda57ebc
Daniela Fernandez <daniela.fernandez7@gmail.com>
2019-03-27 19:26:12 -0300

    correcciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,13 +10,13 @@ $(function(){
     if (this.hash !== "") {
       event.preventDefault();
 
-      var gato = this.hash;
+      var hash = this.hash;
 
 
       $("html, body").animate({
-        scrollTop: $(gato).offset().top
+        scrollTop: $(hash).offset().top
       }, 800, function(){
-        window.location.hash = gato;
+        window.location.hash = hash;
       });
     }
   });
